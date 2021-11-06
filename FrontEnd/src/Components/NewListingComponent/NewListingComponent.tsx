be9d106834e24165f6c47a5a353013da405436a2
GuthrieNick <guthrien99@gmail.com>
2021-11-06 13:11:43 -0400

    minor changes to NewListingComponent

--- FrontEnd/src/Components/NewListingComponent/NewListingComponent.tsx
+++ FrontEnd/src/Components/NewListingComponent/NewListingComponent.tsx
@@ -32,50 +32,50 @@ export const NewListing: React.FC<any> = () => {
   };
 
   let formatAsMoney = (value: string) => {
-    let x: string = "";
-    let decimal: number = -1;
-    let sz: number = 0;
+    let x: string = "",
+        decimal: number = -1, // -1: No decimal, 0: decimal pt, 1: 1 number after point, 2: 2 numbers after point
+        sz: number = 0,
+        index: number = 0;
+
     // Get number of numeric chars - used for placing commas
-    for (let i of value) {
+    for (let i of value)
       if (i == ".") break;
       else if (!isNaN(Number(i))) sz++;
-    }
-
-    let index: number = 0;
 
     // Iterate over every char in value
-    for (let i of value) {
+    for (let char of value) {
       // No decimal and i is decimal point
-      if (decimal === -1 && i === ".") {
-        x += i;
+      if (decimal === -1 && char === ".") {
+        x += char;
         decimal = 0;
         index = sz + 1;
       }
 
       // Nonnumeric character
-      else if (isNaN(Number(i)) || i === " ") continue;
+      else if (isNaN(Number(char)) || char === " ")
+        continue;
+      
       // Decimal has been set
       else if (decimal > -1) {
         decimal++;
-        x += i;
+        x += char;
         if (decimal === 2) break;
       }
 
       // Just a number
       else {
-        x += i;
+        x += char;
         if (++index < sz && (sz - index) % 3 === 0)
             x += ",";
       }
     }
 
-    return decimal === -1
-      ? x + ".00"
-      : decimal === 0
-      ? x + "00"
-      : decimal === 1
-      ? x + "0"
-      : x;
+    switch (decimal) {
+      case -1: return x + ".00";
+      case  0: return x +  "00";
+      case  1: return x +   "0";
+      default: return x;
+    }
   };
 
   let formSubmit = (event: any) => {
