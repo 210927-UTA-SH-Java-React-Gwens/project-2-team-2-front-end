f0f02866926f68d59876c3a8d8f6f47a88e00fee
Virut1316 <54875369+Virut1316@users.noreply.github.com>
2021-11-10 23:16:07 -0500

    Update user email and username working

--- src/Actions/ActionTypes.ts
+++ src/Actions/ActionTypes.ts
@@ -2,6 +2,8 @@ export const GET_USER = 'GET_USER';
 export const LOGIN_USER = 'LOGIN_USER';
 export const CREATE_USER = 'CREATE_USER';
 export const LOGOUT_USER = 'LOGOUT_USER';
+export const UPDATE_USER_USERNAME = 'UPDATE_USER_USERNAME';
+export const UPDATE_USER_EMAIL = 'UPDATE_USER_EMAIL';
 
 export const GET_LISTING = 'GET_LISTING';
 
--- src/Actions/UserActions.ts
+++ src/Actions/UserActions.ts
@@ -1,4 +1,4 @@
-import {GET_USER,LOGIN_USER,CREATE_USER, LOGOUT_USER} from './ActionTypes';
+import {GET_USER,LOGIN_USER,CREATE_USER, LOGOUT_USER,UPDATE_USER_USERNAME, UPDATE_USER_EMAIL} from './ActionTypes';
 import axios from 'axios';
 
 const hostURL = 'http://localhost:8080/';
@@ -161,6 +161,101 @@ export const logOutUser = () => (dispatch:any) => {
         type: LOGOUT_USER,
         payload : noUser
     });
+}
+
+interface IUpdateUserUsername {
+    id: number,
+    username: string
+}
+
+export const updateUserUsername = (user:IUpdateUserUsername) => async (dispatch:any) => {
+
+    try{
+        
+        const res = await axios.post(hostURL + 'user/update-username', user);
+        let updatedUser = {
+
+            id: res.data.id,
+            username:res.data.username,
+            email:res.data.email,
+            funds : res.data.funds,
+            password : res.data.password
+    
+        }
+        if(!updatedUser.id)
+            throw 'Incorrect credentials';
+
+        return dispatch({
+            type: UPDATE_USER_USERNAME,
+            payload : updatedUser
+        });
+
+    }catch(e){
+
+        let failuser = {
+
+            id: -1,
+            username:'',
+            email:'',
+            funds : 0,
+            password : ''
+    
+        }
+
+        return dispatch({
+            type: UPDATE_USER_USERNAME,
+            payload : failuser
+        });
+
+    }
+}
+
+
+interface IUpdateUserEmail {
+    id: number,
+    email: string
+}
+
+export const updateUserEmail = (user:IUpdateUserEmail) => async (dispatch:any) => {
+
+    try{
+        
+        const res = await axios.post(hostURL + 'user/update-email', user);
+        let updatedUser = {
+
+            id: res.data.id,
+            username:res.data.username,
+            email:res.data.email,
+            funds : res.data.funds,
+            password : res.data.password
+    
+        }
+        if(!updatedUser.id)
+            throw 'Incorrect credentials';
+
+        return dispatch({
+            type: UPDATE_USER_EMAIL,
+            payload : updatedUser
+        });
+
+    }catch(e){
+
+        let failuser = {
+
+            id: -1,
+            username:'',
+            email:'',
+            funds : 0,
+            password : ''
+    
+        }
+
+        return dispatch({
+            type: UPDATE_USER_EMAIL,
+            payload : failuser
+        });
+
+    }
 }
 
 
