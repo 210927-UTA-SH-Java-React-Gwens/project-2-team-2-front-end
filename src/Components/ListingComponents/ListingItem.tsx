7648c9fc825f7f677b0d65a063cb9c21f2fc9dc9
William Cocomise <89233920+b5yB@users.noreply.github.com>
2021-11-11 14:56:29 -0600

    Update ListingItem.tsx

--- src/Components/ListingComponents/ListingItem.tsx
+++ src/Components/ListingComponents/ListingItem.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Container,Row,Col,Card,Form,Button} from 'react-bootstrap';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { formatAsMoney } from "./listing";
 
 
