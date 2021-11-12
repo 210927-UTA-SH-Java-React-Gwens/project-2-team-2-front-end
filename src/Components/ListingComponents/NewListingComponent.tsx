fb5505e6996f06612bdc2f9e1fbc34d5663a6cf3
GuthrieNick <guthrien99@gmail.com>
2021-11-12 09:01:23 -0500

    Started refactoring NewListing to take optional default values (untested)

--- src/Components/ListingComponents/NewListingComponent.tsx
+++ src/Components/ListingComponents/NewListingComponent.tsx
@@ -6,7 +6,7 @@ import {
   InputGroup,
   Row,
   Button,
-  CloseButton
+  CloseButton,
 } from "react-bootstrap";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
@@ -15,16 +15,14 @@ import { ImageViewer, Img } from "../ImageViewerComponent/ImageViewerComponent";
 //import "./NewListing.css";
 import "./NewListing.css";
 import axios from "axios";
+import { SERVER_ADDRESS } from "../../server";
 
-
-
-export const NewListing: React.FC<any> = (props?:any) => {
+export const NewListing: React.FC<any> = (props?: any) => {
   useEffect(() => {
     document.title = "Create a new listing | GWENslist";
 
     if (props?.defaults) {
-      if (props.defaults.images)
-        setImages(props.defaults.images);
+      if (props.defaults.images) setImages(props.defaults.images);
     }
   }, []);
 
@@ -33,12 +31,12 @@ export const NewListing: React.FC<any> = (props?:any) => {
 
   // State holding the set of user images
   const [images, setImages] = useState<Img[]>([]);
-  
+
   // State holding count of images so each one has a unique key
   const [imgKey, setImgKey] = useState(0);
 
   // State holding key of image to be removed if closebutton is pressed
-  const [remKey, setRemKey] = useState('');
+  const [remKey, setRemKey] = useState("");
 
   // State holding if the form has been validated
   const [formValidated, setFormValidated] = useState(false);
@@ -46,26 +44,24 @@ export const NewListing: React.FC<any> = (props?:any) => {
   // State holding the simple listing information to send to the backend
   const [formData, setFormData] = useState(new FormData());
 
-
-
   let updateFormData = (name: string, value: any) => {
     formData.set(name, value);
     setFormData(formData);
   };
 
-
-
   let formSubmit = (event: any) => {
     event.preventDefault();
-    
+
     let form = event.currentTarget;
-    if (!form.checkValidity())
-      event.stopPropagation();
+    if (!form.checkValidity()) event.stopPropagation();
     else {
       setFsetDisabled(true);
-        
-      axios.post("http://localhost:8080/listing/new", formData, { headers: {'Content-Type': 'multipart/form-data'}})
-      .then((res) => console.log(res));
+
+      axios
+        .post(SERVER_ADDRESS + "listing/new", formData, {
+          headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then((res) => console.log(res));
 
       (() => new Promise((resolve) => setTimeout(resolve, 3000)))().then(() =>
         setFsetDisabled(false)
@@ -75,56 +71,47 @@ export const NewListing: React.FC<any> = (props?:any) => {
     setFormValidated(true);
   };
 
-
-
-  let addImages = (imgs:Img[]) => {
+  let addImages = (imgs: Img[]) => {
     setImages([...images, ...imgs]);
-    for (let img of imgs)
-      formData.append("images", img.data);
+    for (let img of imgs) formData.append("images", img.data);
     setFormData(formData);
-  }
+  };
 
-  let removeImage = (key : string) => {
-    let imgs:Img[] = [];
+  let removeImage = (key: string) => {
+    let imgs: Img[] = [];
 
     // Reset images to list of images excluding the one with the given key
     for (let image of images)
-      if (image.key === key)
-        continue;
+      if (image.key === key) continue;
       else imgs.push(image);
     setImages(imgs);
 
     // Reset form data images
     formData.delete("images");
-    for (let img of images)
-      formData.append('images', img.data);
+    for (let img of images) formData.append("images", img.data);
     setFormData(formData);
-  }
+  };
 
-
-
-  let parseFiles = (target : HTMLInputElement) => {
+  let parseFiles = (target: HTMLInputElement) => {
     console.log(target.files);
-    let imgs:Img[] =[];
+    let imgs: Img[] = [];
     let key = imgKey;
-    
+
     for (let i = 0; target.files && i < target.files.length; i++) {
-      console.log(i, target.files[i])
+      console.log(i, target.files[i]);
       imgs.push({
         src: URL.createObjectURL(target.files[i]),
         data: target.files[i],
         name: target.files[i].name,
-        key: `img${key++}`
+        key: `img${key++}`,
       });
 
       formData.append("images", target.files[i]);
     }
 
-    setImgKey(key+1); 
+    setImgKey(key + 1);
     addImages(imgs);
-  }
-
-
+  };
 
   return (
     <div>
@@ -140,12 +127,18 @@ export const NewListing: React.FC<any> = (props?:any) => {
                   name="title"
                   placeholder="Listing title"
                   required
+                  defaultValue={props?.title ? props.title : ""}
                   onChange={(e) => updateFormData("title", e.target.value)}
                 />
-                <Form.Control.Feedback type="invalid">You must give your listing a title</Form.Control.Feedback>
-               </Form.Group>
+                <Form.Control.Feedback type="invalid">
+                  You must give your listing a title
+                </Form.Control.Feedback>
+              </Form.Group>
               <Row id="cat-price-row">
-                <CustomCategoryInput update={updateFormData} />
+                <CustomCategoryInput
+                  defaultValue={props?.category ? props.category : ""}
+                  update={updateFormData}
+                />
                 <PriceInput update={updateFormData} />
               </Row>
               {/* Images */}
@@ -157,20 +150,31 @@ export const NewListing: React.FC<any> = (props?:any) => {
                   accept="image/*"
                   multiple
                   required
-                  onChange={e => parseFiles(e.target as HTMLInputElement)} style={{width: "50%"}}/>
-                  <Form.Control.Feedback type="invalid">You must add at least one image</Form.Control.Feedback>
-                <div style={{display: images.length ? "flex" : "none", margin: "1em", height: "100%"}}>
-                  <CloseButton onClick={()=>{ removeImage(remKey); }} />
+                  onChange={(e) => parseFiles(e.target as HTMLInputElement)}
+                  style={{ width: "50%" }}
+                />
+                <Form.Control.Feedback type="invalid">
+                  You must add at least one image
+                </Form.Control.Feedback>
+                <div
+                  style={{
+                    display: images.length ? "flex" : "none",
+                    margin: "1em",
+                    height: "100%",
+                  }}
+                >
+                  <CloseButton
+                    onClick={() => {
+                      removeImage(remKey);
+                    }}
+                  />
                   <ImageViewer images={images} setKey={setRemKey} />
                 </div>
               </Form.Group>
-              
             </Col>
-            <Col id="desc-col" style={{ textAlign: "left"}}>
+            <Col id="desc-col" style={{ textAlign: "left" }}>
               {/* Description */}
-              <label className="form-label">
-                Listing Description
-              </label>
+              <label className="form-label">Listing Description</label>
               <br />
               <ReactQuill
                 id="description"
@@ -183,17 +187,19 @@ export const NewListing: React.FC<any> = (props?:any) => {
           </Row>
         </fieldset>
         {/* Submit */}
-        <Button variant="primary" type="submit">Submit</Button>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
         {/* Cancel */}
-        <Button variant="outline-secondary" type="button">Cancel</Button>
+        <Button variant="outline-secondary" type="button">
+          Cancel
+        </Button>
       </Form>
     </div>
   );
 };
 
-
-
-let CustomCategoryInput : React.FC<any> = (props:any) => {
+let CustomCategoryInput: React.FC<any> = (props: any) => {
   let categories = [
     "Collectibles",
     "Electronics",
@@ -206,80 +212,106 @@ let CustomCategoryInput : React.FC<any> = (props:any) => {
     "Toys",
   ];
 
+  const getDefaultValue = () => {
+    // Check if a default value was passed in
+    if (!(props.defaultValue in ["", null, undefined])) {
+      if (props.defaultValue in categories)
+        // Predefined default category
+        return props.defaultValue;
+
+      // Custom category, set custom category input
+      setCustomView(true);
+      setCustomCat(props.defaultValue);
+      return "Other";
+    }
+
+    // No default value
+    return "";
+  };
+
+  const setCategory = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCustomCat(e.target.value);
+    if (e.target.value.length === 0)
+      props.update("category", "Other");
+    else props.update("category", e.target.value);
+  };
+
   // State for turning on/off the custom category input display
   const [customView, setCustomView] = useState(false);
+
   // State holding the value of the custom category input field
   const [customCat, setCustomCat] = useState("Other");
-  
-  return (
-      <Form.Group as={Col} className="form-group">
-        <Form.Label>Category</Form.Label>
-        <Form.Control as="select"
-        name="category"
-          defaultValue=""
-          onChange={(e) => {
-            if (e.target.value === "Other") {
-              setCustomView(true);
-              props.update("category", customCat);
-            } else {
-              setCustomView(false);
-              props.update("category", e.target.value);
-            }
-          }}
-          required
-        >
-          <option value="" disabled>
-            Select a category
-          </option>
-          {categories.map((cat) => (
-            <option value={cat}>{cat}</option>
-          ))}
-          <option value="Other">
-            Custom (leave blank for "Other")
-          </option>
-        </Form.Control>
-        <Form.Control
-          id="custom-input"
-          style={{
-            display: customView ? "block" : "none",
-            marginTop: "1em",
-          }}
-          type="text"
-          name="custom"
-          placeholder="Custom category..."
-          onChange={(e) => {
-            setCustomCat(e.target.value);
-            if (e.target.value.length === 0)
-              props.update("category", "Other");
-            else props.update("category", e.target.value);
-          }}
-        />
-        <Form.Control.Feedback type="invalid">You must categorize your listing</Form.Control.Feedback>
-      </Form.Group>
-  )
-};
 
-let PriceInput : React.FC<any> = (props:any) => {
   return (
-  <Form.Group as={Col} className="form-group">
-    <Form.Label>Price</Form.Label>
-    <InputGroup>
-      <InputGroup.Text>$</InputGroup.Text>
-      <FormControl
-        type="text"
-        name="price"
-        placeholder="0.00"
-        required
-        onBlur={(e) => {
-          if (e.target.value !== '') {
-            e.target.value = formatAsMoney(e.target.value);
-            console.log(Number(e.target.value.replace(',', '')));
-            props.update("price", Number(e.target.value.replace(',', '')) * 100);
+    <Form.Group as={Col} className="form-group">
+      <Form.Label>Category</Form.Label>
+      <Form.Control
+        as="select"
+        name="category"
+        defaultValue={getDefaultValue()}
+        onChange={(e) => {
+          if (e.target.value === "Other") {
+            setCustomView(true);
+            props.update("category", customCat);
+          } else {
+            setCustomView(false);
+            props.update("category", e.target.value);
           }
         }}
+        required
+      >
+        <option value="" disabled>
+          Select a category
+        </option>
+        {categories.map((cat) => (
+          <option value={cat}>{cat}</option>
+        ))}
+        <option value="Other">Custom (leave blank for "Other")</option>
+      </Form.Control>
+      <Form.Control
+        id="custom-input"
+        style={{
+          display: customView ? "block" : "none",
+          marginTop: "1em",
+        }}
+        type="text"
+        name="custom"
+        placeholder="Custom category..."
+        onChange={setCategory}
       />
-      <Form.Control.Feedback type="invalid">You must set the price of your item</Form.Control.Feedback>
-    </InputGroup>
-  </Form.Group>
-  )
-}
+      <Form.Control.Feedback type="invalid">
+        You must categorize your listing
+      </Form.Control.Feedback>
+    </Form.Group>
+  );
+};
+
+let PriceInput: React.FC<any> = (props: any) => {
+  const setPrice = (e: React.FocusEvent<HTMLInputElement>) => {
+    if (e.target.value !== "") {
+      e.target.value = formatAsMoney(e.target.value);
+      console.log(Number(e.target.value.replace(",", "")));
+      props.update("price", Number(e.target.value.replace(",", "")) * 100);
+    }
+  };
+
+  return (
+    <Form.Group as={Col} className="form-group">
+      <Form.Label>Price</Form.Label>
+      <InputGroup>
+        <InputGroup.Text>$</InputGroup.Text>
+        <FormControl
+          type="text"
+          name="price"
+          placeholder="0.00"
+          defaultValue={props.defaultValue ? props.defaultValue : ""}
+          required
+          onBlur={setPrice}
+        />
+        <Form.Control.Feedback type="invalid">
+          You must set the price of your item
+        </Form.Control.Feedback>
+      </InputGroup>
+    </Form.Group>
+  );
+};
