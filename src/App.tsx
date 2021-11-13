745979d60450e753c7035142af653dbbaf9b59be
GuthrieNick <guthrien99@gmail.com>
2021-11-12 19:35:30 -0500

    ListingView now takes URL parameters and retrieves/displays the listing on the page (not pretty yet)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import { NewListing } from "./Components/ListingComponents/NewListingComponent";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { ListingEditor } from "./Components/ListingComponents/ListingEditorComponent";
+import { HashRouter as Router, Route, Switch, useLocation } from "react-router-dom";
 import { UserComponent } from "./Components/UserComponent/UserComponent";
 import { ListingView } from "./Components/ListingComponents/ListingViewComponent";
 import { Header } from "./Components/HeaderComponent/HeaderComponent";
 import { Home } from "./Components/HomeComponent/Home";
 import { Search } from "./Components/HomeComponent/Search";
 import { MyListingComponent } from "./Components/MyListingComponent/MyListingComponent";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
   useEffect(() => {
@@ -20,17 +23,10 @@ function App() {
       <Header />
         <Switch>
           <Route exact path="/create-listing">
-            <NewListing />
+            <ListingEditor />
           </Route>
           <Route exact path="/listing">
-            <ListingView
-              id="1"
-              title="Title"
-              price="10"
-              desc="Description"
-              poster="Nick"
-              posted="today"
-            />
+            <ListingView/>
           </Route>
           <Route exact path="/user">
             <UserComponent />
