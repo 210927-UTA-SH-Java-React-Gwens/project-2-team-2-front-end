1578717744375eb71d4d695a97d2e1a785143093
GuthrieNick <guthrien99@gmail.com>
2021-11-14 22:43:56 -0500

    Just playing with the listing viewer a bit

--- src/Components/ListingComponents/ListingPreview.tsx
+++ src/Components/ListingComponents/ListingPreview.tsx
@@ -1,11 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import {Container,Row,Col,Card,Form,Button,Modal} from 'react-bootstrap';
-import { Img, Listing } from '../../Store/types';
+import React, { useEffect, useState } from "react";
+import { Col, Card } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { Img, Listing } from "../../Store/types";
 import { formatAsMoney, getPreviewImage } from "./listing";
-import './ListingPreview.css'
-import { ListingView } from './ListingViewComponent';
-
-
+import "./ListingPreview.css";
 
 /*
 id: number,
@@ -17,64 +15,46 @@ id: number,
     category:string
 */
 
+export const ListingPreview: React.FC<any> = (
+  props: { listing: Listing } & any
+) => {
+  const appState = useSelector<any, any>((state) => state);
+  const [mainImage, setMainImage] = useState<Img | null>(null);
 
-export const ListingPreview : React.FC<any> = (props:{listing:Listing}&any) => {
-    const [mainImage, setMainImage] = useState<Img | null>(null);
+  useEffect(() => {
+    getPreviewImage(props.listing.id).then((img: Img) => setMainImage(img));
+  }, []);
 
-    useEffect(() => {
-        getPreviewImage(props.listing.id).then((img:Img) => setMainImage(img));
-    }, [])
-
-
-    return(
-        <Col xs={3}>
-            <Card className='listing-card' onClick={props.onClick ? props.onClick : () => {}}>
-                <Card.Img variant="top" style={{padding: ".5em"}} src={mainImage?.src} />
-                <Card.Body>
-                    <Card.Title>{props.listing.title}</Card.Title>
-                    <Card.Subtitle>{'$' + formatAsMoney(Number(props.listing.price).toString())}</Card.Subtitle>
-                    <Card.Text>In category <b>{props.listing.category}</b></Card.Text>
-                    <Button variant="outline-primary" size="sm">View</Button>
-                    <Button variant="outline-success" size="sm">Message</Button>
-                    <Card.Footer>
-                        <small className="text-muted">Posted by {props.listing.poster.username}<br/> on {new Date(props.listing.posted).toLocaleString()}</small>
-                    </Card.Footer>
-                </Card.Body>
-            </Card>
-        </Col>
-    );
-}
-
-/*
-<Card style={{width: '10rem'}}>
-    <Card.Img variant="top" src=""/>
-    <Card.Body>
-        <Card.Title>{listing.title}</Card.Title>
-        <Card.Subtitle>{'$' + formatAsMoney(Number(listing.price).toString())}</Card.Subtitle>
-        <Card.Text><div className="lv-desc" dangerouslySetInnerHTML={{__html: listing.content}}></div></Card.Text>
-        <Card.Text>Posted by {listing.poster.username}<br/> on {new Date(listing.posted).toLocaleString()}</Card.Text>
-        <Button variant="outline-primary" size="sm">View</Button>
-        <Button variant="outline-success" size="sm">Message</Button>
-    </Card.Body>
-</Card>
-*/
-
-/*
-<Container>
-            <div className="listing-itemized" key={listing.id}>
-                <Row className="align-items-left">
-                    <Col md={3}>
-                        <img className="listing-image" src=""></img>
-                    </Col>
-                    <Col md={3}>
-                        <h5 className="listing-title">{listing.title}</h5>
-                        <p>{'$' + formatAsMoney(Number(listing.price).toString())}</p>
-                        <h6>Posted by {listing.poster.username}<br/> on {new Date(listing.posted).toLocaleString()}</h6>
-                    </Col>
-                    <Col md={6}>
-                        <div className="lv-desc" dangerouslySetInnerHTML={{__html: listing.content}}></div>
-                    </Col>
-                </Row>
-            </div>
-        </Container>
-*/
+  return (
+    <Col xs={3}>
+      <Card
+        className="listing-card"
+        onClick={props.onClick ? props.onClick : () => {}}
+      >
+        <Card.Img
+          variant="top"
+          style={{ padding: ".5em" }}
+          src={mainImage?.src}
+        />
+        <Card.Body>
+          <Card.Title>{props.listing.title}</Card.Title>
+          <Card.Subtitle>
+            {"$" + formatAsMoney(Number(props.listing.price).toString())}
+          </Card.Subtitle>
+          <Card.Text>
+            In category <b>{props.listing.category}</b>
+          </Card.Text>
+          <Card.Footer>
+            <small className="text-muted">
+              Posted by{" "}
+              {props.listing.poster.username === appState.user.username
+                ? "you"
+                : props.listing.poster.username}
+              <br /> on {new Date(props.listing.posted).toLocaleString()}
+            </small>
+          </Card.Footer>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
